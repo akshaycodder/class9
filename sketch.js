3b5a057eafe7c04a4df9c6e55cf996bdbbe370f9
akshaycodder <87016274+akshaycodder@users.noreply.github.com>
2021-07-12 18:25:36 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,25 @@
+
+function setup() {
+  createCanvas(400,400);
+}
+
+function draw() 
+{
+  background(30);
+if(keyDown(DOWN_ARROW))
+ background('red')
+
+ if(keyDown(LEFT_ARROW))
+ background('blue')
+
+ if(keyDown(RIGHT_ARROW))
+ background('pink')
+
+
+ if(keyDown(UP_ARROW))
+ background('green')
+
+}
+
+
+
